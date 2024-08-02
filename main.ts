9fa63b287596346b4eaec34196c2207c5f89c4ca
Nicholas Wilcox <nwilcox@linicks.dev>
2024-08-01 20:04:18 -0500

    Remove default settings code

--- main.ts
+++ main.ts
@@ -1,41 +1,12 @@
-import {
-  App,
-  Editor,
-  MarkdownView,
-  Modal,
-  Notice,
-  Plugin,
-  PluginSettingTab,
-  Setting,
-  normalizePath,
-} from "obsidian";
+import { Plugin } from "obsidian";
 import { render } from "pug";
 
-interface UnofficialPugSettings {
-  mySetting: string;
-}
-
-const DEFAULT_SETTINGS: UnofficialPugSettings = {
-  mySetting: "default",
-};
-
 export default class UnofficialPugPlugin extends Plugin {
-  settings: UnofficialPugSettings;
-
   async onload() {
-    await this.loadSettings();
     this.registerMarkdownCodeBlockProcessor("pug", (source, el, ctx) => {
       el.innerHTML = render(source, { fm: ctx.frontmatter });
     });
   }
 
   onunload() {}
-
-  async loadSettings() {
-    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-  }
-
-  async saveSettings() {
-    await this.saveData(this.settings);
-  }
 }
